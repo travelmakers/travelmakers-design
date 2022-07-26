6c922b378220a4c39c5f79c801036546e19f5d07
Seong Gi Dong <sgd0947@gmail.com>
2022-07-26 11:02:25 +0900

    🐛 fix: delete static folder

--- storybook-static/main.cf8dacb0665279b392c1.manager.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{450:function(n,s,o){o(451),o(830),o(801),o(832),o(829),o(831),o(833),o(827),n.exports=o(828)},497:function(n,s){}},[[450,3,4]]]);
