8a41c2a4892197024908b3476fd625e6bed94579
Seong Gi Dong <sgd0947@gmail.com>
2022-08-23 17:42:31 +0900

    fix(core): styles 참조할 수 있게 코드 추가

--- packages/travelmakers-design-core/src/index.ts
+++ packages/travelmakers-design-core/src/index.ts
@@ -1,1 +1,14 @@
-export * from './components';
+export {
+  createStyles,
+  TmProvider,
+  useTmTheme,
+  ColorSchemeProvider,
+  useTmColorScheme,
+  Global,
+} from "@travelmakers-design/styles";
+export type {
+  TmTheme,
+  Co,
+  CoComponentProps,
+} from "@travelmakers-design/styles";
+export * from "./components";
