d748a99ed5d7d5d1787596b059ce8dacea6888c2
Seong Gi Dong <sgd0947@gmail.com>
2022-11-28 13:02:58 +0900

    feat(component): created a component CarouselTitleCard
    WEB-71

---
+++ packages/travelmakers-design-core/src/components/CarouselTitleCard/index.ts
@@ -0,0 +1,2 @@
+export { CarouselTitleCard } from "./CarouselTitleCard";
+export type { CarouselTitleCardProps } from "./CarouselTitleCard";
---
+++ packages/travelmakers-design-core/src/components/CarouselTitleCard/stories/CarouselTitleCard.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { CarouselTitleCard } from "../CarouselTitleCard";
+
+export default {
+  title: "@travelmakers-design/core/Component/CarouselTitleCard",
+  component: CarouselTitleCard,
+  argTypes: {
+    newBadge: {
+      defaultValue: false,
+      description: "CarouselTitleCard 컴포넌트의 newBadge 여부를 판단합니다.",
+      table: {
+        type: {
+          summary: "boolean",
+        },
+      },
+      control: { type: "boolean" },
+    },
+    subHeadline: {
+      defaultValue: "서브헤드라인",
+      description: "CarouselTitleCard 컴포넌트의 서브헤드라인을 표시합니다.",
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+      control: { type: "text" },
+    },
+    headline: {
+      defaultValue: "헤드라인",
+      description: "CarouselTitleCard 컴포넌트의 헤드라인을 표시합니다.",
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+      control: { type: "text" },
+    },
+    description: {
+      defaultValue: "디스크립션",
+      description: "CarouselTitleCard 컴포넌트의 디스크립션을 표시합니다.",
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+      control: { type: "text" },
+    },
+  },
+};
+
+export const Default = (props) => {
+  return (
+    <div style={{ padding: 24 }}>
+      <CarouselTitleCard {...props} />
+    </div>
+  );
+};
