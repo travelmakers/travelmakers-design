7033379224b069d805e0dca2ed318e0c915fd52b
Seong Gi Dong <sgd0947@gmail.com>
2022-12-02 19:08:40 +0900

    feat: url settings

--- config/rollup/create-package-config.ts
+++ config/rollup/create-package-config.ts
@@ -1,15 +1,18 @@
-import path from 'path';
-import fs from 'fs-extra';
-import { RollupOptions, OutputOptions, ModuleFormat } from 'rollup';
-import commonjs from '@rollup/plugin-commonjs';
-import nodeExternals from 'rollup-plugin-node-externals';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import esbuild from 'rollup-plugin-esbuild';
-import json from '@rollup/plugin-json';
-import alias, { Alias } from '@rollup/plugin-alias';
-import replace from '@rollup/plugin-replace';
-import visualizer from 'rollup-plugin-visualizer';
-import { getPackagesList } from '../../scripts/utils/get-packages-list';
+import path from "path";
+import fs from "fs-extra";
+import { RollupOptions, OutputOptions, ModuleFormat } from "rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeExternals from "rollup-plugin-node-externals";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import esbuild from "rollup-plugin-esbuild";
+import image from "@rollup/plugin-image";
+import svgr from "@svgr/rollup";
+import json from "@rollup/plugin-json";
+import url from "rollup-plugin-url";
+import alias, { Alias } from "@rollup/plugin-alias";
+import replace from "@rollup/plugin-replace";
+import visualizer from "rollup-plugin-visualizer";
+import { getPackagesList } from "../../scripts/utils/get-packages-list";
 
 interface PkgConfigInput {
   basePath: string;
@@ -22,32 +25,41 @@ interface PkgConfigInput {
   analyze: boolean;
 }
 
-export default async function createPackageConfig(config: PkgConfigInput): Promise<RollupOptions> {
-  const packageJson = JSON.parse(fs.readFileSync(path.join(config.basePath, './package.json')).toString('utf-8'));
+export default async function createPackageConfig(
+  config: PkgConfigInput
+): Promise<RollupOptions> {
+  const packageJson = JSON.parse(
+    fs
+      .readFileSync(path.join(config.basePath, "./package.json"))
+      .toString("utf-8")
+  );
   const pkgList = await getPackagesList();
 
   const aliasEntries: Alias[] = pkgList.map((pkg) => ({
     find: new RegExp(`^${pkg.packageJson.name}`),
-    replacement: path.resolve(pkg.path, 'src'),
+    replacement: path.resolve(pkg.path, "src"),
   }));
 
   const plugins = [
     commonjs(),
     nodeExternals(),
-    nodeResolve({ extensions: ['.ts', '.tsx', '.js', '.jsx'] }),
+    nodeResolve({ extensions: [".ts", ".tsx", ".js", ".jsx"] }),
     esbuild({
-      minify: config.format === 'umd',
+      minify: config.format === "umd",
       sourceMap: false,
-      tsconfig: path.resolve(process.cwd(), 'tsconfig.json'),
+      tsconfig: path.resolve(process.cwd(), "tsconfig.json"),
     }),
     json(),
     alias({ entries: aliasEntries }),
     replace({ preventAssignment: true }),
+    url(),
+    image(),
+    svgr(),
   ];
 
   let externals;
 
-  if (config.format === 'umd') {
+  if (config.format === "umd") {
     externals = [
       ...(config?.externals || []),
       ...Object.keys({
@@ -56,7 +68,7 @@ export default async function createPackageConfig(config: PkgConfigInput): Promi
     ];
   } else {
     externals = [
-      '@emotion/server/create-instance',
+      "@emotion/server/create-instance",
       ...(config?.externals || []),
       ...Object.keys({
         ...packageJson.peerDependencies,
@@ -72,53 +84,56 @@ export default async function createPackageConfig(config: PkgConfigInput): Promi
     sourcemap: config.sourcemap,
   };
 
-  if (config.format === 'es') {
-    output.dir = path.resolve(config.basePath, 'esm');
+  if (config.format === "es") {
+    output.dir = path.resolve(config.basePath, "esm");
     output.preserveModules = true;
   }
 
-  if (config.format === 'cjs') {
-    output.dir = path.resolve(config.basePath, 'cjs');
+  if (config.format === "cjs") {
+    output.dir = path.resolve(config.basePath, "cjs");
     output.preserveModules = true;
-    output.exports = 'named';
+    output.exports = "named";
   }
 
-  if (config.format === 'umd') {
-    output.file = path.resolve(config.basePath, 'lib/index.umd.js');
+  if (config.format === "umd") {
+    output.file = path.resolve(config.basePath, "lib/index.umd.js");
     output.globals = {
       ...pkgList
         .map((pkg) => ({
           [pkg.packageJson.name]: pkg.packageJson.name,
         }))
         .reduce((globals, pkgGlobal) => ({ ...globals, ...pkgGlobal }), {}),
-      react: 'React',
-      dayjs: 'dayjs',
-      'react-dom': 'ReactDOM',
+      react: "React",
+      dayjs: "dayjs",
+      "react-dom": "ReactDOM",
     };
   }
 
-  if (config.analyze && config.format === 'umd') {
+  if (config.analyze && config.format === "umd") {
     plugins.push(
       visualizer({
         title: packageJson.name,
-        filename: path.join(config.basePath, 'lib/stats.html'),
-        projectRoot: path.join(config.basePath, 'src'),
+        filename: path.join(config.basePath, "lib/stats.html"),
+        projectRoot: path.join(config.basePath, "src"),
         sourcemap: true,
         gzipSize: true,
       }),
       visualizer({
         title: packageJson.name,
-        filename: path.join(config.basePath, 'lib/stats.json'),
-        projectRoot: path.join(config.basePath, 'src'),
+        filename: path.join(config.basePath, "lib/stats.json"),
+        projectRoot: path.join(config.basePath, "src"),
         json: true,
         sourcemap: true,
         gzipSize: true,
-      }),
+      })
     );
   }
 
   return {
-    input: config?.entry || packageJson.source ? path.resolve(config.basePath, packageJson.source) : path.resolve(config.basePath, 'src/index.ts'),
+    input:
+      config?.entry || packageJson.source
+        ? path.resolve(config.basePath, packageJson.source)
+        : path.resolve(config.basePath, "src/index.ts"),
     output,
     external: externals,
     plugins,
