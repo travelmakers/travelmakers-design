5e13e6b6d0a77f182a3c4875e5590b7c76d7e1bc
Seong Gi Dong <sgd0947@gmail.com>
2022-12-21 11:16:14 +0900

    feat(utils): created a getTimeLineStateDetailFunc function

---
+++ packages/travelmakers-design-utils/src/getTimeLineStateDetailFunc/getTimeLineStateDetailFunc.ts
@@ -0,0 +1,406 @@
+import { getTimeStamp, getTimeStampCheckIn } from "../utils/getTimeStamp";
+
+import { getCountDown } from "../utils/getCountDown";
+
+type ReservationState =
+  | "default"
+  | "tour_confirm_before"
+  | "tour_confirm"
+  | "tour_done"
+  | "reservation_purchase_before"
+  | "reservation_purchase_done"
+  | "checkin_before"
+  | "day_n"
+  | "extend_purchase_before"
+  | "extend_purchase_done"
+  | "extend_checkin_before"
+  | "reservation_change_process"
+  | "checkout_before"
+  | "checkout_before_n"
+  | "checkout_done"
+  | "tour_cancel"
+  | "reservation_cancel";
+
+export interface timeLineStateParams {
+  enum: ReservationState;
+  thirdLineText: (hotelName: string) => string;
+  link: (url?: string) => {
+    url: string;
+    arrow: boolean;
+  };
+}
+
+interface timeLineStateParamsWithExpectedDate extends timeLineStateParams {
+  firstLineText: (state?: string) => string;
+  secondLineText: (expectedDate: string) => string;
+}
+
+interface timeLineStateParamsWithStartDate extends timeLineStateParams {
+  firstLineText: (state?: string) => string;
+  secondLineText: (start_date: string) => string;
+}
+
+interface timeLineStateParamsWithEndDate extends timeLineStateParams {
+  firstLineText: (state?: string) => string;
+  secondLineText: (end_date: string) => string;
+}
+
+interface timeLineStateParamsWithEndDateDday extends timeLineStateParams {
+  firstLineText: (dDay: number | string) => string;
+  secondLineText: (end_date: string) => string;
+}
+
+interface timeLineStateParamsWithBankDate extends timeLineStateParams {
+  firstLineText: (state?: string) => string;
+  secondLineText: (vbank_date: string) => string;
+}
+
+interface timeLineStateParamsWithNull extends timeLineStateParams {
+  firstLineText: (state?: string) => string;
+  secondLineText: () => string;
+  secondLink: (url?: string) => string;
+}
+
+interface timeLineStateParamsWithCancelDate extends timeLineStateParams {
+  firstLineText: (state?: string) => string;
+  secondLineText: (cancelDate) => string;
+}
+
+type AllTypes =
+  | timeLineStateParamsWithExpectedDate
+  | timeLineStateParamsWithStartDate
+  | timeLineStateParamsWithEndDate
+  | timeLineStateParamsWithEndDateDday
+  | timeLineStateParamsWithBankDate
+  | timeLineStateParamsWithNull
+  | timeLineStateParamsWithCancelDate;
+
+export function getTimeLineStateDetailFunc(
+  t:
+    | "tour_confirm_before"
+    | "reservation_purchase_done"
+    | "extend_purchase_done"
+    | "reservation_change_process"
+): timeLineStateParamsWithExpectedDate;
+
+export function getTimeLineStateDetailFunc(
+  t: "tour_confirm" | "tour_done" | "checkin_before" | "extend_checkin_before"
+): timeLineStateParamsWithStartDate;
+
+export function getTimeLineStateDetailFunc(
+  t: "day_n"
+): timeLineStateParamsWithEndDate;
+
+export function getTimeLineStateDetailFunc(
+  t: "checkout_before" | "checkout_before_n"
+): timeLineStateParamsWithEndDateDday;
+
+export function getTimeLineStateDetailFunc(
+  t: "reservation_purchase_before" | "extend_purchase_before"
+): timeLineStateParamsWithBankDate;
+
+export function getTimeLineStateDetailFunc(
+  t: "checkout_done"
+): timeLineStateParamsWithNull;
+
+export function getTimeLineStateDetailFunc(
+  t: "tour_cancel" | "reservation_cancel"
+): timeLineStateParamsWithCancelDate;
+
+export function getTimeLineStateDetailFunc(state: ReservationState): AllTypes {
+  switch (state) {
+    case "tour_confirm_before":
+      // NOTE: 투어 확정 전
+      return {
+        enum: state,
+        firstLineText: () => `투어 확정 전`,
+        secondLineText: (expectedDate) =>
+          `${getTimeStamp(new Date(expectedDate))} 이내 확정 예정`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "tour_confirm":
+      // NOTE: 투어 확정
+      return {
+        enum: state,
+        firstLineText: () => `투어 확정`,
+        secondLineText: (startDate) =>
+          `${getTimeStamp(new Date(startDate), true)} 방문`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "tour_done":
+      // NOTE: 투어 완료
+      return {
+        enum: state,
+        firstLineText: () => `투어 완료`,
+        secondLineText: (startDate) =>
+          `${getTimeStamp(new Date(startDate), true)} 방문`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "reservation_purchase_before":
+      // NOTE: 결제 진행 중 (가상 계좌)
+      return {
+        enum: state,
+        firstLineText: () => `결제 진행 중`,
+        secondLineText: (vbank_date) =>
+          `${getCountDown(vbank_date)} 내 입금 필요`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "extend_purchase_before":
+      // NOTE: 연장 결제 전
+      return {
+        enum: state,
+        firstLineText: () => `연장 결제 전`,
+        secondLineText: (vbank_date) =>
+          `${getCountDown(vbank_date)} 내 입금 필요`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "reservation_purchase_done":
+      // NOTE: 예악 확정 전
+      return {
+        enum: state,
+        firstLineText: () => `예악 확정 전`,
+        secondLineText: (expectedDate) =>
+          `${getTimeStamp(new Date(expectedDate))} 이내 확정`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "checkin_before":
+      // NOTE: 체크인 전
+      return {
+        enum: state,
+        firstLineText: () => `체크인 D-Day`,
+        secondLineText: (startDate) =>
+          `${getTimeStamp(new Date(startDate), true)} 체크인`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "day_n":
+      // NOTE: 입주 N일차
+      return {
+        enum: state,
+        firstLineText: () => `호텔 이용 중`,
+        secondLineText: (endDate) =>
+          `${getTimeStamp(new Date(endDate), true)} 체크아웃`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "extend_purchase_done":
+      // NOTE: 연장확정전
+      return {
+        enum: state,
+        firstLineText: () => `연장 확정 전`,
+        secondLineText: (expectedDate) =>
+          `${getTimeStamp(new Date(expectedDate))} 이내 확정 예정`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "extend_checkin_before":
+      // NOTE: 연장확정
+      return {
+        enum: state,
+        firstLineText: () => `연장 확정`,
+        secondLineText: (startDate) =>
+          `${getTimeStamp(new Date(startDate), true)} 체크인`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "reservation_change_process":
+      // NOTE: 예약 변경 중
+      return {
+        enum: state,
+        firstLineText: () => `예약 변경 중`,
+        secondLineText: (expectedDate) =>
+          `${getTimeStamp(new Date(expectedDate))} 이내 확정`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "checkout_before":
+      // NOTE: 체크아웃 전
+      return {
+        enum: state,
+        firstLineText: (dDay) => `체크아웃 D-${dDay}`,
+        secondLineText: (end_date) =>
+          `${getTimeStamp(new Date(end_date), true)} 체크아웃`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "checkout_before_n":
+      // NOTE: 체크아웃 N일 전
+      return {
+        enum: state,
+        firstLineText: (dDay) => `체크아웃 D-${dDay}`,
+        secondLineText: (end_date) =>
+          `${getTimeStamp(new Date(end_date), true)} 체크아웃`,
+        thirdLineText: (hotelName) => hotelName,
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "checkout_done":
+      // NOTE: 체크아웃
+      return {
+        enum: state,
+        firstLineText: () => `체크아웃 완료`,
+        secondLineText: () => `재구매하기`,
+        secondLink: (url) => url,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "tour_cancel":
+      // NOTE: 투어 취소
+      return {
+        enum: state,
+        firstLineText: () => `투어 취소`,
+        secondLineText: (cancelDate) =>
+          `${getTimeStamp(new Date(cancelDate), true)} 방문 취소`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    case "reservation_cancel":
+      // NOTE: 예약 취소 완료(환불O)
+      return {
+        enum: state,
+        firstLineText: () => `취소 완료`,
+        secondLineText: (cancelDate) =>
+          `${getTimeStamp(new Date(cancelDate), true)} 방문 취소`,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+    default:
+      return {
+        enum: state,
+        firstLineText: () => ``,
+        secondLineText: () => ``,
+        thirdLineText: (hotelName) => hotelName,        
+        link: (url?: string) => {
+          return { url, arrow: !!url };
+        },
+      };
+  }
+}
+
+type Props = {
+  first: {
+    dDay?: number;
+  };
+  second: {
+    userName?: string;
+    hotelName?: string;
+    expectedDate?: string;
+    startDate?: string;
+    cancelDate?: string;
+    endDate?: string;
+    vBankDate?: string;
+    dDay?: number;
+  };
+  third?: {
+    hotelName?: string;
+  };
+  roomTypeText?: {
+    roomType?: string;
+  };
+};
+
+interface TimeLineStatePropsReturnType {
+  first: string;
+  second: string;
+  third: string;  
+}
+
+export function getTimeLineStateProps(
+  state: ReservationState,
+  props: Props
+): TimeLineStatePropsReturnType {
+  switch (state) {
+    case "checkout_before" || "checkout_before_n":
+      return {
+        first: `${props.first.dDay}`,
+        second: props.second.endDate,
+        third: props.third.hotelName,        
+      };
+    case "tour_confirm_before" ||
+      "reservation_purchase_done" ||
+      "extend_purchase_done" ||
+      "reservation_change_process":
+      return {
+        first: null,
+        second: props.second.expectedDate,
+        third: props.third.hotelName,        
+      };
+    case "tour_confirm" ||
+      "tour_done" ||
+      "checkin_before" ||
+      "extend_checkin_before":
+      return {
+        first: null,
+        second: props.second.startDate,
+        third: props.third.hotelName,        
+      };
+    case "day_n":
+      return {
+        first: null,
+        second: props.second.endDate,
+        third: props.third.hotelName,        
+      };
+    case "reservation_purchase_before" || "extend_purchase_before":
+      return {
+        first: null,
+        second: props.second.vBankDate,
+        third: props.third.hotelName,        
+      };
+    case "checkout_done":
+      return {
+        first: null,
+        second: null,
+        third: props.third.hotelName,        
+      };
+    case "tour_cancel" || "reservation_cancel":
+      return {
+        first: null,
+        second: props.second.cancelDate,
+        third: props.third.hotelName,        
+      };
+
+    default:
+      return {
+        first: null,
+        second: null,
+        third: props.third.hotelName,        
+      };
+  }
+}
