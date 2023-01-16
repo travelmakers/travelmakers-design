fd7e50325646f72999969870aa6b5416b29b3583
Seong Gi Dong <sgd0947@gmail.com>
2023-01-16 19:28:13 +0900

    fix(utils): fixed a getPayMethod func

--- packages/travelmakers-design-utils/src/getPayMethod/getPayMethod.tsx
+++ packages/travelmakers-design-utils/src/getPayMethod/getPayMethod.tsx
@@ -8,22 +8,26 @@ const getPayMethod = (
   card_name?: string,
   card_quota?: number
 ) => {
-  const cardDescription = card_quota
-    ? card_quota === 0
-      ? "(일시불)"
-      : "(" + card_quota + "개월 할부)"
-    : "";
+  const getCardDescription = () => {
+    if (card_quota === 0) {
+      return "(일시불)";
+    } else if (card_quota > 0) {
+      return "(" + card_quota + "개월 할부)";
+    }
+    return "";
+  };
+
   switch (pay_method) {
     case "card":
-      return `${card_name ?? "신용카드"}${cardDescription}`;
+      return `${card_name ?? "신용카드"}${getCardDescription()}`;
     case "vbank":
       return `무통장입금 (가상계좌)`;
     case "naverpay":
-      return `네이버페이${cardDescription}`;
+      return `네이버페이${getCardDescription()}`;
     case "kakaopay":
-      return `카카오페이${cardDescription}`;
+      return `카카오페이${getCardDescription()}`;
     case "samsung":
-      return `삼성페이${cardDescription}`;
+      return `삼성페이${getCardDescription()}`;
 
     default:
       return "";
